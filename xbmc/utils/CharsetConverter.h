5ecaa79ad48c4adf56da7b9788ffcbb1bab52c58
Elan Feingold <elan@bluemandrill.com>
2008-06-20 19:58:20 -1000

    [OS X] Changed to use llvm-gcc

--- xbmc/utils/CharsetConverter.h
+++ xbmc/utils/CharsetConverter.h
@@ -93,9 +93,9 @@ public:
   void logicalToVisualBiDi(const CStdStringA& strSource, CStdStringA& strDest, FriBidiCharSet fribidiCharset, FriBidiCharType base = FRIBIDI_TYPE_LTR);
   void logicalToVisualBiDi(const CStdStringA& strSource, CStdStringA& strDest, CStdStringA& charset, FriBidiCharType base = FRIBIDI_TYPE_LTR);
 
-  void CCharsetConverter::GuiCharsetTo(const CStdStringA& strDestCharset, CStdStringA& strSourceDest);
+  void GuiCharsetTo(const CStdStringA& strDestCharset, CStdStringA& strSourceDest);
 
-  void CCharsetConverter::GuiCharsetTo(const CStdStringA& strDestCharset, const CStdStringA& strSource, CStdStringA& strDest);
+  void GuiCharsetTo(const CStdStringA& strDestCharset, const CStdStringA& strSource, CStdStringA& strDest);
 
 private:
   std::vector<CStdString> m_vecCharsetNames;
