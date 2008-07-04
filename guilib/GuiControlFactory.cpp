48745159e58dbc474ee8b708156c3c659fbd32fc
jmarshallnz <jmarshallnz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2008-07-04 02:48:32 +0000

    changed: Added <resetonlabelchange> tag to fadelabels, which resets the scrolling on a change of label.  Defaults to true.  Those who wish to have the previous behaviour should add this tag and set it false (only useful for things like playtime in the fadelabel really).
    git-svn-id: http://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@13914 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- guilib/GuiControlFactory.cpp
+++ guilib/GuiControlFactory.cpp
@@ -691,6 +691,7 @@ CGUIControl* CGUIControlFactory::Create(DWORD dwParentId, const FRECT &rect, TiX
   CPoint camera;
   bool   hasCamera = false;
   int scrollSpeed = CScrollInfo::defaultSpeed;
+  bool resetOnLabelChange = true;
 
   /////////////////////////////////////////////////////////////////////////////
   // Read control properties from XML
@@ -995,6 +996,7 @@ CGUIControl* CGUIControlFactory::Create(DWORD dwParentId, const FRECT &rect, TiX
 
   XMLUtils::GetBoolean(pControlNode, "usecontrolcoords", useControlCoords);
   XMLUtils::GetBoolean(pControlNode, "renderfocusedlast", renderFocusedLast);
+  XMLUtils::GetBoolean(pControlNode, "resetonlabelchange", resetOnLabelChange);
 
   // view type
   VIEW_TYPE viewType = VIEW_TYPE_NONE;
@@ -1105,7 +1107,7 @@ CGUIControl* CGUIControlFactory::Create(DWORD dwParentId, const FRECT &rect, TiX
   {
     control = new CGUIFadeLabelControl(
       dwParentId, id, posX, posY, width, height,
-      labelInfo, scrollOut, scrollSpeed, timeToPauseAtEnd);
+      labelInfo, scrollOut, scrollSpeed, timeToPauseAtEnd, resetOnLabelChange);
 
     ((CGUIFadeLabelControl *)control)->SetInfo(infoLabels);
   }
