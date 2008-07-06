3d22daab5f448ececaf52ffb70c7af394723ac17
jmarshallnz <jmarshallnz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2008-07-06 01:48:12 +0000

    added: New setting "Stack Duplicate TV Shows" to stack duplicate tvshows into one when there is files from multiple paths. added: Always flatten tv shows down to the episode level option.  Thanks to kraqh3d.
    git-svn-id: http://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@13958 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowSettingsCategory.cpp
+++ xbmc/GUIWindowSettingsCategory.cpp
@@ -762,6 +762,14 @@ void CGUIWindowSettingsCategory::CreateSettings()
       pControl->AddLabel(g_localizeStrings.Get(13143), VS_SKIPLOOP_BIDIR);
       pControl->AddLabel(g_localizeStrings.Get(13144), VS_SKIPLOOP_NONKEY);
       pControl->AddLabel(g_localizeStrings.Get(13145), VS_SKIPLOOP_ALL);
+    }
+    else if (strSetting.Equals("videolibrary.flattentvshows"))
+    {
+      CSettingInt *pSettingInt = (CSettingInt*)pSetting;
+      CGUISpinControlEx *pControl = (CGUISpinControlEx *)GetControl(GetSetting(strSetting)->GetID());
+      pControl->AddLabel(g_localizeStrings.Get(20420), 0); // Never
+      pControl->AddLabel(g_localizeStrings.Get(20421), 1); // One Season
+      pControl->AddLabel(g_localizeStrings.Get(20422), 2); // Always
       pControl->SetValue(pSettingInt->GetData());
     }
 #ifdef __APPLE__
@@ -2489,6 +2497,11 @@ void CGUIWindowSettingsCategory::OnClick(CBaseSettingControl *pSettingControl)
   else if (strSetting.Equals("lookandfeel.skinzoom"))
   {
     g_fontManager.ReloadTTFFonts();
+  }
+  else if (strSetting.Equals("videolibrary.flattentvshows") ||
+           strSetting.Equals("videolibrary.removeduplicates"))
+  {
+    CUtil::DeleteVideoDatabaseDirectoryCache();
   }
 
   UpdateSettings();
