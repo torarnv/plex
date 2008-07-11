5a46c4a2beefe37f2eacd5868aa04363ee951ad7
jmarshallnz <jmarshallnz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2008-07-11 09:58:49 +0000

    fixed: Largeimage control didn't work for non-skin images.
    git-svn-id: http://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@14006 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUILargeTextureManager.cpp
+++ xbmc/GUILargeTextureManager.cpp
@@ -61,8 +61,8 @@ void CGUILargeTextureManager::Process()
     CFileItem file(path, false);
     if (file.IsPicture() && !(file.IsZIP() || file.IsRAR() || file.IsCBR() || file.IsCBZ())) // ignore non-pictures
     { // check for filename only (i.e. lookup in skin/media/)
-      CStdString loadPath;
-      if (path.FindOneOf("/\\") == CStdString::npos)
+      CStdString loadPath(path);
+      if ((size_t)path.FindOneOf("/\\") == CStdString::npos)
       {
         loadPath = CUtil::AddFileToFolder(g_graphicsContext.GetMediaDir(), "media");
         loadPath = CUtil::AddFileToFolder(loadPath, path);
