b952961d0abc4b4234de28a141d4c201341d7f65
elupus <elupus@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2008-07-22 22:31:29 +0000

    fixed: buffering dialog took long to to go away after starting playback
    git-svn-id: http://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@14366 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDPlayer.cpp
+++ xbmc/cores/dvdplayer/DVDPlayer.cpp
@@ -848,14 +848,7 @@ void CDVDPlayer::Process()
 
       // if we are caching, start playing it agian
       if (m_caching && !m_bAbortRequest)
-      {
         SetCaching(false);
-        if (m_pDlgCache)
-        {
-          m_pDlgCache->Close();
-          m_pDlgCache = NULL;
-        }
-      }
 
       // while players are still playing, keep going to allow seekbacks
       if(m_dvdPlayerAudio.m_messageQueue.GetDataSize() > 0 
@@ -1646,6 +1639,12 @@ void CDVDPlayer::SetCaching(bool enabled)
     m_dvdPlayerAudio.SetSpeed(m_playSpeed);
     m_dvdPlayerVideo.SetSpeed(m_playSpeed);
     m_caching = false;
+
+    if (m_pDlgCache)
+    {
+      m_pDlgCache->Close();
+      m_pDlgCache = NULL;
+    }
   }
 }
 
