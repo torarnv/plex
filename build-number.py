d6f5f1f05481c1b7737e5e95b51e6f604c4d3967
Elan Feingold <elan@bluemandrill.com>
2008-07-26 00:40:38 -1000

    Automagic version in about box and Info.plist

--- build-number.py
+++ build-number.py
@@ -4,10 +4,9 @@ import os, sys
 from Foundation import NSMutableDictionary
 
 version = os.popen4("/opt/local/bin/git rev-parse HEAD")[1].read().strip()
-version = "%s (%s)" % (sys.argv[1], version[0:7])
+git_rev = version[0:7]
 info = os.environ['BUILT_PRODUCTS_DIR']+"/"+os.environ['WRAPPER_NAME']+"/Contents/Info.plist"
 
 plist = NSMutableDictionary.dictionaryWithContentsOfFile_(info)
-plist['CFBundleVersion'] = "Version " + version
-plist['CFBundleShortVersionString'] = sys.argv[1]
+plist['CFBundleVersion'] = git_rev
 plist.writeToFile_atomically_(info, 1)
--- xbmc/osx/PlexDelegate.h
+++ xbmc/osx/PlexDelegate.h
@@ -12,5 +12,6 @@
 {
 }
 - (float)appNameLabelFontSize;
+- (NSString*)versionString;
 
 @end
