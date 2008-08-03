043f68869f34a4253afd71fe8e6fdec54f7960b1
elupus <elupus@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2008-08-03 17:34:07 +0000

    changed: even if OpenFile() succeded, still post delayed events if player still doesn't return IsPlaying(). Could happen if file is extremly short.
    git-svn-id: http://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@14666 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -5040,7 +5040,8 @@ bool CApplication::PlayFile(const CFileItem& item, bool bRestart)
     if (!g_guiSettings.GetBool("lookandfeel.soundsduringplayback"))
       g_audioManager.Enable(false);
   }
-  else
+
+  if(!bResult || !IsPlaying())
   {
     // since we didn't manage to get playback started, send any queued up messages
     while(m_vPlaybackStarting.size())
