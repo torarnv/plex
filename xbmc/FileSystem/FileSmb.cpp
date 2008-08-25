svn2github/xbmc4xbox

changed: allow FileSMB to stat open files. (libdvdnav does so to figure out what type of file it has opened)
git-svn-id: https://svn.exotica.org.uk:8443/xbmc4xbox@14769 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

branches/linuxport/XBMC/xbmc/FileSystem/FileSmb.cpp
@@ -517,6 +517,35 @@ bool CFileSMB::Exists(const CURL& url)
   return true;
 }
 
+int CFileSMB::Stat(struct __stat64* buffer)
+{
+  if (m_fd == -1)
+    return -1;
+
+#ifndef _LINUX
+  struct __stat64 tmpBuffer = {0};
+#else
+  struct stat tmpBuffer = {0};
+#endif
+
+  CSingleLock lock(smb);
+  int iResult = smbc_fstat(m_fd, &tmpBuffer);
+
+  buffer->st_dev = tmpBuffer.st_dev;
+  buffer->st_ino = tmpBuffer.st_ino;  
+  buffer->st_mode = tmpBuffer.st_mode;
+  buffer->st_nlink = tmpBuffer.st_nlink;
+  buffer->st_uid = tmpBuffer.st_uid;
+  buffer->st_gid = tmpBuffer.st_gid;
+  buffer->st_rdev = tmpBuffer.st_rdev;
+  buffer->st_size = tmpBuffer.st_size;
+  buffer->st_atime = tmpBuffer.st_atime;
+  buffer->st_mtime = tmpBuffer.st_mtime;
+  buffer->st_ctime = tmpBuffer.st_ctime;
+
+  return iResult;
+}
+
 int CFileSMB::Stat(const CURL& url, struct __stat64* buffer)
 {
   smb.Init();
@@ -528,7 +557,7 @@ int CFileSMB::Stat(const CURL& url, struct __stat64* buffer)
 #ifndef _LINUX
   struct __stat64 tmpBuffer = {0};
 #else
-  struct stat tmpBuffer;
+  struct stat tmpBuffer = {0};
 #endif
   int iResult = smbc_stat(strFileName, &tmpBuffer);
 

torarnv/plex

git-svn-id: http://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@14769 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/FileSystem/FileSmb.cpp
@@ -527,6 +527,35 @@ bool CFileSMB::Exists(const CURL& url)
   return true;
 }
 
+int CFileSMB::Stat(struct __stat64* buffer)
+{
+  if (m_fd == -1)
+    return -1;
+
+#ifndef _LINUX
+  struct __stat64 tmpBuffer = {0};
+#else
+  struct stat tmpBuffer = {0};
+#endif
+
+  CSingleLock lock(smb);
+  int iResult = smbc_fstat(m_fd, &tmpBuffer);
+
+  buffer->st_dev = tmpBuffer.st_dev;
+  buffer->st_ino = tmpBuffer.st_ino;  
+  buffer->st_mode = tmpBuffer.st_mode;
+  buffer->st_nlink = tmpBuffer.st_nlink;
+  buffer->st_uid = tmpBuffer.st_uid;
+  buffer->st_gid = tmpBuffer.st_gid;
+  buffer->st_rdev = tmpBuffer.st_rdev;
+  buffer->st_size = tmpBuffer.st_size;
+  buffer->_st_atime = tmpBuffer.st_atime;
+  buffer->_st_mtime = tmpBuffer.st_mtime;
+  buffer->_st_ctime = tmpBuffer.st_ctime;
+
+  return iResult;
+}
+
 int CFileSMB::Stat(const CURL& url, struct __stat64* buffer)
 {
   smb.Init();
@@ -537,7 +566,7 @@ int CFileSMB::Stat(const CURL& url, struct __stat64* buffer)
 #ifndef _LINUX
   struct __stat64 tmpBuffer = {0};
 #else
-  struct stat tmpBuffer;
+  struct stat tmpBuffer = {0};
 #endif
   int iResult = smbc_stat(strFileName, &tmpBuffer);
 