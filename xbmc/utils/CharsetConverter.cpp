c44c44ac488c24b71560889c60b585a67aa5ccf2
Elan Feingold <elan@bluemandrill.com>
2008-09-01 14:37:42 -1000

    Reenable flipping now that threading issue is fixed

--- xbmc/utils/CharsetConverter.cpp
+++ xbmc/utils/CharsetConverter.cpp
@@ -226,7 +226,6 @@ void CCharsetConverter::utf8ToW(const CStdStringA& utf8String, CStdStringW &wStr
 {
   CStdStringA strFlipped;
 
-#if 0
   // Try to flip hebrew/arabic characters, if any
   if (bVisualBiDiFlip)
   {
@@ -234,7 +233,6 @@ void CCharsetConverter::utf8ToW(const CStdStringA& utf8String, CStdStringW &wStr
     convert(m_iconvUtf8toW,sizeof(wchar_t),"UTF-8",WCHAR_CHARSET,strFlipped,wString);
   }
   else
-#endif
     convert(m_iconvUtf8toW,sizeof(wchar_t),"UTF-8",WCHAR_CHARSET,utf8String,wString);
 }
 
