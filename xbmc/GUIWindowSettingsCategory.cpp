ce6c870ec54fb86dcc50b3b743c93a62ce8f8065
jmarshallnz <jmarshallnz@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2008-09-08 04:15:20 +0000

    fixed: Dialog to check if the resolution was valid (Test Resolution button in settings) failed to autoclose.
    git-svn-id: http://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@15455 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowSettingsCategory.cpp
+++ xbmc/GUIWindowSettingsCategory.cpp
@@ -1515,9 +1515,8 @@ void CGUIWindowSettingsCategory::OnClick(CBaseSettingControl *pSettingControl)
     g_graphicsContext.SetVideoResolution(m_NewResolution, TRUE);
     g_guiSettings.m_LookAndFeelResolution = m_NewResolution;
     g_application.ReloadSkin();
-    CGUIDialogYesNo dlg;
-    dlg.SetAutoClose(5000);
-    if (!dlg.ShowAndGetInput(13110, 13111, 20022, 20022))
+    bool cancelled = false;
+    if (!CGUIDialogYesNo::ShowAndGetInput(13110, 13111, 20022, 20022, -1, -1, cancelled, 5000))
     {
       g_guiSettings.SetInt("videoscreen.resolution", lastRes);
       g_graphicsContext.SetVideoResolution(lastRes, TRUE);
