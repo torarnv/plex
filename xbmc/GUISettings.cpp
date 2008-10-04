d1ba6d5fd102616020fa8b94bbf485d18be14f33
iordonez <ordonezi@mac.com>
2008-10-04 02:00:13 -0700

    Removed Treat Stacked files as a single File from GUI

--- xbmc/GUISettings.cpp
+++ xbmc/GUISettings.cpp
@@ -526,7 +526,7 @@ CGUISettings::CGUISettings(void)
   // video settings
   AddGroup(5, 3);
   AddCategory(5, "myvideos", 16000);
-  AddBool(1, "myvideos.treatstackasfile", 20051, true);
+  AddBool(0, "myvideos.treatstackasfile", 20051, true);
   AddInt(2, "myvideos.resumeautomatically", 12017, RESUME_ASK, RESUME_NO, 1, RESUME_ASK, SPIN_CONTROL_TEXT);
   AddBool(3, "myvideos.autothumb",12024, false);
   AddBool(4, "myvideos.cleanfilenames", 20418, false);
