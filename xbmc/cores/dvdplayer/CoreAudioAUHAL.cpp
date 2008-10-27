205c09fb0f7bde03c73aea4a02b64e93de35a0ec
Ryan Walklin <ryan@Crackotage.(none)>
2008-10-27 22:08:52 +1300

    Fixed bug in SPDIF code - now tested working

--- xbmc/cores/dvdplayer/CoreAudioAUHAL.cpp
+++ xbmc/cores/dvdplayer/CoreAudioAUHAL.cpp
@@ -981,49 +981,53 @@ int CoreAudioAUHAL::OpenSPDIF(struct CoreAudioDeviceParameters *deviceParameters
     if( !AudioStreamChangeFormat(deviceParameters, deviceParameters->i_stream_id, deviceParameters->stream_format))
         return false;
 	
+<<<<<<< HEAD:xbmc/cores/dvdplayer/CoreAudioAUHAL.cpp
 	
 	// Get device hardware buffer size
 	
 	uint32_t audioDeviceLatency, audioStreamLatency, audioDeviceBufferFrameSize, audioDeviceSafetyOffset;
 	deviceParameters->hardwareFrameLatency = 0.0;
+	i_param_size = sizeof(uint32_t);
 	
-	AudioDeviceGetProperty(deviceParameters->device_id, 
+	err = AudioDeviceGetProperty(deviceParameters->device_id, 
 						   0, false, 
 						   kAudioDevicePropertyLatency, 
 						   &i_param_size, 
 						   &audioDeviceLatency);
 	
-	deviceParameters->hardwareFrameLatency += audioDeviceLatency;
+	if (err == noErr) deviceParameters->hardwareFrameLatency += audioDeviceLatency;
 	
-	AudioDeviceGetProperty(deviceParameters->device_id, 
+	err = AudioDeviceGetProperty(deviceParameters->device_id, 
 						   0, false, 
 						   kAudioDevicePropertyBufferFrameSize, 
 						   &i_param_size, 
 						   &audioDeviceBufferFrameSize);
 	
-	deviceParameters->hardwareFrameLatency += audioDeviceBufferFrameSize;
+	if (err == noErr) deviceParameters->hardwareFrameLatency += audioDeviceBufferFrameSize;
 	
-	AudioDeviceGetProperty(deviceParameters->device_id, 
+	err = AudioDeviceGetProperty(deviceParameters->device_id, 
 						   0, false, 
 						   kAudioDevicePropertySafetyOffset, 
 						   &i_param_size, 
 						   &audioDeviceSafetyOffset);
 	
-	deviceParameters->hardwareFrameLatency += audioDeviceSafetyOffset;
+	if (err == noErr) deviceParameters->hardwareFrameLatency += audioDeviceSafetyOffset;
 	
-	AudioStreamGetProperty(deviceParameters->i_stream_id, 
+	err = AudioStreamGetProperty(deviceParameters->i_stream_id, 
 						   0, 
 						   kAudioStreamPropertyLatency, 
 						   &i_param_size, 
 						   &audioStreamLatency);
 	
-	deviceParameters->hardwareFrameLatency += audioStreamLatency;
+	if (err == noErr) deviceParameters->hardwareFrameLatency += audioStreamLatency;
 	
 	
 	CLog::Log(LOGINFO, "Hardware latency: %.0f frames (%.2f msec @ %.0fHz)", deviceParameters->hardwareFrameLatency, 
 			  deviceParameters->hardwareFrameLatency / deviceParameters->stream_format.mSampleRate * 1000,
 			  deviceParameters->stream_format.mSampleRate);
 	
+=======
+>>>>>>> b4436b5... Implement hardware latency for SPDIF output (untested):xbmc/cores/dvdplayer/CoreAudioAUHAL.cpp
   	// initialise the CoreAudio sink buffer
 	uint32_t framecount = 1;
 	while(framecount <= deviceParameters->stream_format.mSampleRate) // ensure power of 2
