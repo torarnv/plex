730c700e9108a2c820a4c466fe3f1705d9517d94
Elan Feingold <elan@bluemandrill.com>
2008-11-10 20:13:02 -1000

    Allow letter/number at end (e.g. 0.7.0a1)

--- build-number.py
+++ build-number.py
@@ -6,6 +6,6 @@ git_rev = version[0:7]
 info = os.environ['BUILT_PRODUCTS_DIR']+"/"+os.environ['WRAPPER_NAME']+"/Contents/Info.plist"
 
 plist = open(info).read()
-shortVersion = re.search('<string>([0-9]+\.[0-9]+\.[0-9]+)</string>', plist)
+shortVersion = re.search('<string>([0-9]+\.[0-9]+\.[0-9]+(a[0-9]+)?)</string>', plist)
 plist = plist.replace('BUNDLE_VERSION', shortVersion.group(1) + '-' + git_rev)
 open(info, "w").write(plist)
