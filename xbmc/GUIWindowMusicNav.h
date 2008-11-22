928b28540f81f9f11ddb25d2cdb5e0f7ec8432e9
spiff_ <spiff_@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2008-11-22 03:11:44 +0000

    added: possibility to map scan for new content / scan item to library to keymap. action is scanitem.
    git-svn-id: http://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@16272 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/GUIWindowMusicNav.h
+++ xbmc/GUIWindowMusicNav.h
@@ -34,6 +34,7 @@ public:
   virtual ~CGUIWindowMusicNav(void);
 
   virtual bool OnMessage(CGUIMessage& message);
+  virtual bool OnAction(const CAction& action);
   virtual void Render();
 
   virtual void ClearFileItems();
