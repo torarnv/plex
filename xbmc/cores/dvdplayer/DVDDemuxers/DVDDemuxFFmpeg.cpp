fc52a09c40bb5b5dc57496fe33dae2fd2ee42d49
elupus <elupus@568bbfeb-2a22-0410-94d2-cc84cf5bfa90>
2009-04-29 16:59:01 +0000

    changed: only do partial reads during probing of streamed content to avoid any requirement to seek back
    git-svn-id: http://xbmc.svn.sourceforge.net/svnroot/xbmc/branches/linuxport/XBMC@19830 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

--- xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxFFmpeg.cpp
+++ xbmc/cores/dvdplayer/DVDDemuxers/DVDDemuxFFmpeg.cpp
@@ -337,7 +337,11 @@ bool CDVDDemuxFFmpeg::Open(CDVDInputStream* pInput)
       pd.filename = strFile.c_str();
 
       // read data using avformat's buffers
-      pd.buf_size = m_dllAvFormat.get_buffer(m_ioContext, pd.buf, sizeof(probe_buffer));
+      if(context->is_streamed)
+        pd.buf_size = m_dllAvFormat.get_partial_buffer(m_ioContext, pd.buf, sizeof(probe_buffer));
+      else
+        pd.buf_size = m_dllAvFormat.get_buffer(m_ioContext, pd.buf, sizeof(probe_buffer));
+
       if (pd.buf_size == 0)
       {
         CLog::Log(LOGERROR, "%s - error reading from input stream, %s", __FUNCTION__, strFile.c_str());
