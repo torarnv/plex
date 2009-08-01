dc6590ed9ad43112f91ba7bec3b0898dd04d5f96
Elan Feingold <elan@bluemandrill.com>
2009-08-01 01:04:31 -1000

    Cleaned up and improved the background loader code

--- xbmc/BackgroundInfoLoader.cpp
+++ xbmc/BackgroundInfoLoader.cpp
@@ -34,18 +34,24 @@
 CBackgroundInfoLoader::CBackgroundInfoLoader(int nThreads, int pauseBetweenLoadsInMS)
 {
   m_bStop = true;
-  m_pObserver=NULL;
-  m_pProgressCallback=NULL;
+  m_pObserver = NULL;
+  m_pProgressCallback = NULL;
   m_pVecItems = NULL;
   m_nRequestedThreads = nThreads;
-  m_bStartCalled = false;
-  m_nActiveThreads = 0;
   m_pauseBetweenLoadsInMS = pauseBetweenLoadsInMS;
+  m_workerGroup = 0;
 }
 
 CBackgroundInfoLoader::~CBackgroundInfoLoader()
 {
   StopThread();
+  
+  if (m_workerGroup)
+  {
+    m_workerGroup->Stop();
+    m_workerGroup->Detach();
+    m_workerGroup = 0;
+  }
 }
 
 void CBackgroundInfoLoader::SetNumOfWorkers(int nThreads)
@@ -53,68 +59,42 @@ void CBackgroundInfoLoader::SetNumOfWorkers(int nThreads)
   m_nRequestedThreads = nThreads;
 }
 
-void CBackgroundInfoLoader::Run()
+void CBackgroundRunner::Process()
 {
   try
   {
-    if (m_vecItems.size() > 0)
+    while (m_bStop == false)
     {
+      CFileItemPtr pItem = m_group.PopItem();
+      if (pItem)
       {
-        CSingleLock lock(m_lock);
-        if (!m_bStartCalled)
-        {
-          OnLoaderStart();
-          m_bStartCalled = true;
-        }
+        // Load the item.
+        try { m_group.LoadItem(pItem); }
+        catch (...) { CLog::Log(LOGERROR, "%s::LoadItem - Unhandled exception for item %s", __FUNCTION__, pItem->m_strPath.c_str()); }
       }
-      
-      while (!m_bStop)
+      else
       {
-        CSingleLock lock(m_lock);
-        CFileItemPtr pItem;
-        std::vector<CFileItemPtr>::iterator iter = m_vecItems.begin();
-        if (iter != m_vecItems.end())
-        {
-          pItem = *iter;
-          m_vecItems.erase(iter);
-        }
-
-        if (pItem == NULL)
-          break;
-
-        // Ask the callback if we should abort
-        if (m_pProgressCallback && m_pProgressCallback->Abort() || m_bStop)
-          break;
-
-        lock.Leave();
-        try
-        {
-          // Load an item.
-          if (LoadItem(pItem.get()) && m_pObserver)
-            m_pObserver->OnItemLoaded(pItem.get());
-          
-          // Pause if it was requested.
-          if (m_pauseBetweenLoadsInMS > 0)
-            ::usleep(m_pauseBetweenLoadsInMS*1000);
-        }
-        catch (...)
-        {
-          CLog::Log(LOGERROR, "%s::LoadItem - Unhandled exception for item %s", __FUNCTION__, pItem->m_strPath.c_str());
-        }
+        // We're done!
+        break;
       }
     }
-
-    CSingleLock lock(m_lock);
-    if (m_nActiveThreads == 1)
-      OnLoaderFinish();
-    m_nActiveThreads--;
-
   }
   catch (...)
   {
-    m_nActiveThreads--;
     CLog::Log(LOGERROR, "%s - Unhandled exception", __FUNCTION__);
   }
+
+  // We're done.
+  m_group.WorkerDone();
+}
+
+void CBackgroundInfoLoader::OnLoaderFinished()
+{
+  EnterCriticalSection(m_lock);
+  m_workerGroup = 0;
+  LeaveCriticalSection(m_lock);
+  
+  OnLoaderFinish();
 }
 
 void CBackgroundInfoLoader::Load(CFileItemList& items)
@@ -130,9 +110,8 @@ void CBackgroundInfoLoader::Load(CFileItemList& items)
     m_vecItems.push_back(items[nItem]);
 
   m_pVecItems = &items;
-  m_bStop = false;
-  m_bStartCalled = false;
 
+  // Compute how many threads to spawn.
   int nThreads = m_nRequestedThreads;
   if (nThreads == -1)
     nThreads = (m_vecItems.size() / (ITEMS_PER_THREAD+1)) + 1;
@@ -140,46 +119,42 @@ void CBackgroundInfoLoader::Load(CFileItemList& items)
   if (nThreads > MAX_THREAD_COUNT)
     nThreads = MAX_THREAD_COUNT;
 
-  m_nActiveThreads = nThreads;
-  for (int i=0; i < nThreads; i++)
-  {
-    CThread *pThread = new CThread(this); 
-    pThread->Create();
-#ifndef _LINUX
-    pThread->SetPriority(THREAD_PRIORITY_BELOW_NORMAL);
-#endif
-    pThread->SetName("Background Loader");
-    m_workers.push_back(pThread);
-  }
+  if (items.IsPlexMediaServer())
+    nThreads = 2;
+  
+  // Create a worker group.
+  m_bStop = false;
+  m_workerGroup = new CBackgroundRunnerGroup(this, items, nThreads, m_pauseBetweenLoadsInMS);
       
   LeaveCriticalSection(m_lock);
 }
 
 void CBackgroundInfoLoader::StopAsync()
 {
+  EnterCriticalSection(m_lock);
+  
+  if (m_workerGroup)
+  {
+    // Tell it to stop and then forget about it.
+    m_workerGroup->Stop();
+    m_workerGroup = 0;
+  }
+ 
   m_bStop = true;
+  
+  LeaveCriticalSection(m_lock);
 }
 
 
 void CBackgroundInfoLoader::StopThread()
 {
   StopAsync();
-
-  for (int i=0; i<(int)m_workers.size(); i++)
-  {
-    m_workers[i]->StopThread();
-    delete m_workers[i];
-  }
-
-  m_workers.clear();
-  m_vecItems.clear();
-  m_pVecItems = NULL;
-  m_nActiveThreads = 0;
 }
 
 bool CBackgroundInfoLoader::IsLoading()
 {
-  return m_nActiveThreads > 0;
+  CSingleLock lock(m_lock);
+  return m_workerGroup != 0 && m_workerGroup->GetNumActiveThreads() > 0;
 }
 
 void CBackgroundInfoLoader::SetObserver(IBackgroundLoaderObserver* pObserver)
--- xbmc/BackgroundInfoLoader.h
+++ xbmc/BackgroundInfoLoader.h
@@ -24,12 +24,16 @@
 #include "utils/Thread.h"
 #include "IProgressCallback.h"
 #include "utils/CriticalSection.h"
+#include "FileItem.h"
 
 #include <vector>
 #include "boost/shared_ptr.hpp"
+#include "boost/foreach.hpp"
 
-class CFileItem; typedef boost::shared_ptr<CFileItem> CFileItemPtr;
+class CFileItem; 
+typedef boost::shared_ptr<CFileItem> CFileItemPtr;
 class CFileItemList;
+class CBackgroundRunnerGroup;
 
 class IBackgroundLoaderObserver
 {
@@ -38,7 +42,7 @@ public:
   virtual void OnItemLoaded(CFileItem* pItem) = 0;
 };
 
-class CBackgroundInfoLoader : public IRunnable
+class CBackgroundInfoLoader
 {
 public:
   CBackgroundInfoLoader(int nThreads=-1, int pauseBetweenLoadsInMS=0);
@@ -46,7 +50,6 @@ public:
 
   void Load(CFileItemList& items);
   bool IsLoading();
-  virtual void Run();
   void SetObserver(IBackgroundLoaderObserver* pObserver);
   void SetProgressCallback(IProgressCallback* pCallback);
   virtual bool LoadItem(CFileItem* pItem) { return false; };
@@ -55,25 +58,196 @@ public:
   void StopAsync();  // will ask loader to stop as soon as possible, but not block
 
   void SetNumOfWorkers(int nThreads); // -1 means auto compute num of required threads
+  
+  IProgressCallback* GetProgressCallback() { return m_pProgressCallback; }
+  IBackgroundLoaderObserver* GetObserver() { return m_pObserver; }
 
-protected:
+  void OnLoaderFinished();
   virtual void OnLoaderStart() {};
   virtual void OnLoaderFinish() {};
+  
+protected:
 
   CFileItemList *m_pVecItems;
   std::vector<CFileItemPtr> m_vecItems; // FileItemList would delete the items and we only want to keep a reference.
   CCriticalSection m_lock;
 
-  bool m_bStartCalled;
   bool m_bRunning;
   volatile bool m_bStop;
   int  m_nRequestedThreads;
-  int  m_nActiveThreads;
   int  m_pauseBetweenLoadsInMS;
 
   IBackgroundLoaderObserver* m_pObserver;
   IProgressCallback* m_pProgressCallback;
 
   std::vector<CThread *> m_workers;
+  
+  CBackgroundRunnerGroup* m_workerGroup;
 };
 
+typedef boost::shared_ptr<CFileItemList> CFileItemListPtr;
+typedef boost::shared_ptr<CCriticalSection> CCriticalSectionPtr;
+
+/////////////////////////////////////////////////////////////////////////////
+class CBackgroundRunnerGroup;
+class CBackgroundRunner : public CThread
+{
+ public:
+  
+  CBackgroundRunner(CBackgroundRunnerGroup& group)
+    : m_group(group)
+    , m_bStop(false)
+  {
+    SetName("Background Runner");
+    Create(true);
+  }
+  
+  virtual ~CBackgroundRunner() {} 
+  
+  void Stop()
+  {
+    m_bStop = true;
+  }
+  
+  virtual void Process();
+  
+ private:
+  
+  CBackgroundRunnerGroup& m_group;
+  volatile bool           m_bStop;
+};
+
+/////////////////////////////////////////////////////////////////////////////
+class CBackgroundInfoLoader;
+class CBackgroundRunnerGroup
+{
+ public:
+  CBackgroundRunnerGroup(CBackgroundInfoLoader* loader, CFileItemList& items, int numThreads, int msBetweenLoads)
+    : m_msBetweenLoads(msBetweenLoads)
+    , m_nActiveThreads(numThreads)
+    , m_loader(loader)
+    , m_stopped(false)
+  {
+    if (items.Size() > 0)
+    {
+      // Copy the list.
+      for (int n=0; n<items.Size(); n++)
+        m_vecItems.push_back(items[n]);
+      
+      // Notify of the start.
+      m_loader->OnLoaderStart();
+      
+      // Start the threads.
+      for (int i=0; i<numThreads; i++)
+      {
+        CBackgroundRunner* runner = new CBackgroundRunner(*this);
+        m_runners.push_back(runner);
+      }
+    }
+  }
+  
+  ~CBackgroundRunnerGroup()
+  {
+    CSingleLock lock(m_lock);
+  }
+  
+  void Stop()
+  {
+    CSingleLock lock(m_lock);
+    m_stopped = true;
+    
+    BOOST_FOREACH(CBackgroundRunner* runner, m_runners)
+      runner->Stop();
+  }
+  
+  void Detach()
+  {
+    CSingleLock lock(m_lock);
+    m_loader = 0;
+  }
+  
+  CFileItemPtr PopItem()
+  {
+    // Pull the first item off the stack.
+    CFileItemPtr pItem;
+    
+    CSingleLock lock(m_lock);
+    std::vector<CFileItemPtr>::iterator iter = m_vecItems.begin();
+    if (iter != m_vecItems.end())
+    {
+      pItem = *iter;
+      m_vecItems.erase(iter);
+    }
+
+    return pItem;
+  }
+  
+  bool LoadItem(CFileItemPtr& item)
+  {
+    EnterCriticalSection(m_lock);
+    if (m_loader && m_stopped == false)
+    {
+      if (m_loader->GetProgressCallback() == 0 || m_loader->GetProgressCallback()->Abort() == false)
+      {
+        CBackgroundInfoLoader* loader = m_loader;
+
+        // Leave the critical section to load the item.
+        LeaveCriticalSection(m_lock);
+        bool ret = loader->LoadItem(item.get());
+        EnterCriticalSection(m_lock);
+        
+        if (ret)
+        {
+          // Notify the observer.
+          NotifyObserver(item);
+          
+          // Pause if it was requested.
+          if (m_msBetweenLoads > 0)
+            ::usleep(m_msBetweenLoads*1000);
+        }
+      }
+    }
+    
+    LeaveCriticalSection(m_lock);
+    return false;
+  }
+  
+  void NotifyObserver(CFileItemPtr& item)
+  {
+    CSingleLock lock(m_lock);
+    if (m_stopped == false && m_loader->GetObserver())
+      m_loader->GetObserver()->OnItemLoaded(item.get());
+  }
+  
+  void WorkerDone()
+  {
+    EnterCriticalSection(m_lock);
+    
+    // If we're the last one on, turn the lights off.
+    if (--m_nActiveThreads == 0)
+    {
+      if (m_loader)
+        m_loader->OnLoaderFinished();
+      
+      LeaveCriticalSection(m_lock);
+      delete this;
+    }
+    else
+    {
+      LeaveCriticalSection(m_lock);
+    }
+  }
+  
+  int GetNumActiveThreads() 
+  { 
+    return m_nActiveThreads; 
+  }
+  
+  CBackgroundInfoLoader*           m_loader;
+  int                              m_msBetweenLoads;
+  int                              m_nActiveThreads;
+  std::vector<CFileItemPtr>        m_vecItems;
+  CCriticalSection                 m_lock;
+  std::vector<CBackgroundRunner* > m_runners;
+  bool                             m_stopped;
+};
