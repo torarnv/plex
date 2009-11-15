torarnv/plex

commit d2d45fd3075708ff05030300a8b07cf133c92cd6

changed: more consistent item/path checks. made them use CURL instead of string compares removed: unneeded memory unit checks
git-svn-id: http://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@24593 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/Util.cpp
@@ -743,7 +743,7 @@ bool CUtil::HasSlashAtEnd(const CStdString& strFile)
 
 bool CUtil::IsRemote(const CStdString& strFile)
 {
-  if (IsMemCard(strFile) || IsCDDA(strFile) || IsISO9660(strFile) || IsPlugin(strFile))
+  if (IsCDDA(strFile) || IsISO9660(strFile) || IsPlugin(strFile))
     return false;
 
   if (IsSpecial(strFile))
@@ -941,26 +941,38 @@ bool CUtil::IsZIP(const CStdString& strFile) // also checks for comic books!
 
 bool CUtil::IsSpecial(const CStdString& strFile)
 {
+  CURL url(strFile);
   return strFile.Left(8).Equals("special:");
 }
 
 bool CUtil::IsPlugin(const CStdString& strFile)
 {
-  return strFile.Left(7).Equals("plugin:");
+  CURL url(strFile);
+  return url.GetProtocol().Equals("plugin") && !url.GetFileName().IsEmpty();
+}
+
+bool CUtil::IsPluginRoot(const CStdString& strFile)
+{
+  CURL url(strFile);
+  return url.GetProtocol().Equals("plugin") && url.GetFileName().IsEmpty();
 }
 
 bool CUtil::IsCDDA(const CStdString& strFile)
 {
-  return strFile.Left(5).Equals("cdda:");
+  CURL url(strFile);
+  return url.GetProtocol().Equals("cdda") && !url.GetFileName().IsEmpty();
 }
 
 bool CUtil::IsISO9660(const CStdString& strFile)
 {
-  return strFile.Left(8).Equals("iso9660:");
+  CURL url(strFile);
+  return url.GetProtocol().Equals("iso9660");
 }
 
 bool CUtil::IsSmb(const CStdString& strFile)
 {
+  CURL url(strFile);
+  return url.GetProtocol().Equals("iso9660");
   return strFile.Left(4).Equals("smb:");
 }
 
@@ -972,11 +984,6 @@ bool CUtil::IsDAAP(const CStdString& strFile)
 bool CUtil::IsUPnP(const CStdString& strFile)
 {
   return strFile.Left(5).Equals("upnp:");
-}
-
-bool CUtil::IsMemCard(const CStdString& strFile)
-{
-  return strFile.Left(3).Equals("mem");
 }
 
 bool CUtil::IsTuxBox(const CStdString& strFile)
@@ -1013,6 +1020,30 @@ bool CUtil::IsLiveTV(const CStdString& strFile)
     return true;
 
   return false;
+}
+
+bool CUtil::IsMusicDb(const CStdString& strFile)
+{
+  CURL url(strFile);
+  return url.GetProtocol().Equals("musicdb");
+}
+
+bool CUtil::IsVideoDb(const CStdString& strFile)
+{
+  CURL url(strFile);
+  return url.GetProtocol().Equals("videodb");
+}
+
+bool CUtil::IsShoutCast(const CStdString& strFile)
+{
+  CURL url(strFile);
+  return url.GetProtocol().Equals("shout");
+}
+
+bool CUtil::IsLastFM(const CStdString& strFile)
+{
+  CURL url(strFile);
+  return url.GetProtocol().Equals("lastfm");
 }
 
 bool CUtil::ExcludeFileOrFolder(const CStdString& strFileOrFolder, const CStdStringArray& regexps)

xbmc/Util.h
@@ -98,14 +98,18 @@ public:
   static bool IsInArchive(const CStdString& strFile);
   static bool IsSpecial(const CStdString& strFile);
   static bool IsPlugin(const CStdString& strFile); 
+  static bool IsPluginRoot(const CStdString& strFile); 
   static bool IsCDDA(const CStdString& strFile);
-  static bool IsMemCard(const CStdString& strFile);
   static bool IsTuxBox(const CStdString& strFile);
   static bool IsMythTV(const CStdString& strFile);
   static bool IsHDHomeRun(const CStdString& strFile);
   static bool IsVTP(const CStdString& strFile);
   static bool IsHTSP(const CStdString& strFile);
   static bool IsLiveTV(const CStdString& strFile);
+  static bool IsMusicDb(const CStdString& strFile);
+  static bool IsVideoDb(const CStdString& strFile);
+  static bool IsShoutCast(const CStdString& strFile);
+  static bool IsLastFM(const CStdString& strFile);
   static bool ExcludeFileOrFolder(const CStdString& strFileOrFolder, const CStdStringArray& regexps);
   static void GetFileAndProtocol(const CStdString& strURL, CStdString& strDir);
   static int GetDVDIfoTitle(const CStdString& strPathFile);

xbmc/FileItem.cpp
@@ -476,13 +476,7 @@ bool CFileItem::IsVideo() const
   if( m_contenttype.Left(6).Equals("video/") )
     return true;
 
-  if (m_strPath.Left(7).Equals("tuxbox:"))
-    return true;
-
-  if (m_strPath.Left(10).Equals("hdhomerun:"))
-    return true;
-
-  if (m_strPath.Left(4).Equals("dvd:"))
+  if (IsHDHomeRun() || IsTuxBox() || CUtil::IsDVD(m_strPath))
     return true;
 
   CStdString extension;
@@ -578,27 +572,22 @@ bool CFileItem::IsPicture() const
 
 bool CFileItem::IsLyrics() const
 {
-  CStdString strExtension;
-  CUtil::GetExtension(m_strPath, strExtension);
-  return (strExtension.CompareNoCase(".cdg") == 0 || strExtension.CompareNoCase(".lrc") == 0);
+  return CUtil::GetExtension(m_strPath).Equals(".cdg", false) || CUtil::GetExtension(m_strPath).Equals(".lrc", false);
 }
 
 bool CFileItem::IsCUESheet() const
 {
-  CStdString strExtension;
-  CUtil::GetExtension(m_strPath, strExtension);
-  return (strExtension.CompareNoCase(".cue") == 0);
+  return CUtil::GetExtension(m_strPath).Equals(".cue", false);
 }
 
 bool CFileItem::IsShoutCast() const
 {
-  if (strstr(m_strPath.c_str(), "shout:") ) return true;
-  return false;
+  return CUtil::IsShoutCast(m_strPath);
 }
+
 bool CFileItem::IsLastFM() const
 {
-  if (strstr(m_strPath.c_str(), "lastfm:") ) return true;
-  return false;
+  return CUtil::IsLastFM(m_strPath);
 }
 
 bool CFileItem::IsInternetStream() const
@@ -718,15 +707,12 @@ bool CFileItem::IsDVDFile(bool bVobs /*= true*/, bool bIfos /*= true*/) const
 
 bool CFileItem::IsRAR() const
 {
-  CStdString strExtension;
-  CUtil::GetExtension(m_strPath, strExtension);
-  if ( (strExtension.CompareNoCase(".rar") == 0) || ((strExtension.Equals(".001") && m_strPath.Mid(m_strPath.length()-7,7).CompareNoCase(".ts.001"))) ) return true; // sometimes the first rar is named .001
-  return false;
+  return CUtil::IsRAR(m_strPath);
 }
 
 bool CFileItem::IsZIP() const
 {
-  return CUtil::GetExtension(m_strPath).Equals(".zip", false);
+  return CUtil::IsZIP(m_strPath);
 }
 
 bool CFileItem::IsCBZ() const
@@ -746,14 +732,12 @@ bool CFileItem::IsStack() const
 
 bool CFileItem::IsPlugin() const
 {
-  CURL url(m_strPath);
-  return url.GetProtocol().Equals("plugin") && !url.GetFileName().IsEmpty();
+  return CUtil::IsPlugin(m_strPath);
 }
 
 bool CFileItem::IsPluginRoot() const
 {
-  CURL url(m_strPath);
-  return url.GetProtocol().Equals("plugin") && url.GetFileName().IsEmpty();
+  return CUtil::IsPluginRoot(m_strPath);
 }
 
 bool CFileItem::IsMultiPath() const
@@ -833,14 +817,14 @@ bool CFileItem::IsHD() const
 
 bool CFileItem::IsMusicDb() const
 {
-  if (strstr(m_strPath.c_str(), "musicdb:") ) return true;
-  return false;
+  CURL url(m_strPath);
+  return url.GetProtocol().Equals("musicdb");
 }
 
 bool CFileItem::IsVideoDb() const
 {
-  if (strstr(m_strPath.c_str(), "videodb:") ) return true;
-  return false;
+  CURL url(m_strPath);
+  return url.GetProtocol().Equals("videodb");
 }
 
 bool CFileItem::IsVirtualDirectoryRoot() const
@@ -848,15 +832,9 @@ bool CFileItem::IsVirtualDirectoryRoot() const
   return (m_bIsFolder && m_strPath.IsEmpty());
 }
 
-bool CFileItem::IsMemoryUnit() const
-{
-  CURL url(m_strPath);
-  return url.GetProtocol().Left(3).Equals("mem");
-}
-
 bool CFileItem::IsRemovable() const
 {
-  return IsOnDVD() || IsCDDA() || IsMemoryUnit() || m_iDriveType == CMediaSource::SOURCE_TYPE_REMOVABLE;
+  return IsOnDVD() || IsCDDA() || m_iDriveType == CMediaSource::SOURCE_TYPE_REMOVABLE;
 }
 
 bool CFileItem::IsReadOnly() const

xbmc/FileItem.h
@@ -126,7 +126,6 @@ public:
   void SetCanQueue(bool bYesNo);
   bool IsParentFolder() const;
   bool IsFileFolder() const;
-  bool IsMemoryUnit() const;
   bool IsRemovable() const;
   bool IsTuxBox() const;
   bool IsMythTV() const;

xbmc/VideoDatabase.cpp
@@ -6637,7 +6637,6 @@ void CVideoDatabase::CleanDatabase(IVideoInfoScannerObserver* pObserver, const v
       // delete all removable media + ftp/http streams
       CURL url(fullPath);
       if (CUtil::IsOnDVD(fullPath) ||
-          CUtil::IsMemCard(fullPath) ||
           url.GetProtocol() == "http" ||
           url.GetProtocol() == "https" ||
           !CFile::Exists(fullPath))

commit 0f6492151ffb30bb02218a4986297c3190cb54d9

fixed: CUtil::IsSmb() was broken since r24593. Changed: More Is-Functions in line with r24593. Added: Stack detection to Is-functions where it makes sense
git-svn-id: http://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@24641 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/Util.cpp
@@ -814,14 +814,19 @@ bool CUtil::IsOnLAN(const CStdString& strPath)
 {
   if(IsMultiPath(strPath))
     return CUtil::IsOnLAN(CMultiPathDirectory::GetFirstPath(strPath));
+
   if(IsStack(strPath))
     return CUtil::IsOnLAN(CStackDirectory::GetFirstStackedFile(strPath));
+
   if(IsSpecial(strPath))
     return CUtil::IsOnLAN(CSpecialProtocol::TranslatePath(strPath));
+
   if(IsDAAP(strPath))
     return true;
+
   if(IsTuxBox(strPath))
     return true;
+
   if(IsUPnP(strPath))
     return true;
 
@@ -867,8 +872,9 @@ bool CUtil::IsOnLAN(const CStdString& strPath)
 
 bool CUtil::IsMultiPath(const CStdString& strPath)
 {
-  if (strPath.Left(12).Equals("multipath://")) return true;
-  return false;
+  CURL url(strPath);
+
+  return url.GetProtocol() == "multipath";
 }
 
 bool CUtil::IsDVD(const CStdString& strFile)
@@ -888,14 +894,16 @@ bool CUtil::IsDVD(const CStdString& strFile)
 
 bool CUtil::IsVirtualPath(const CStdString& strFile)
 {
-  if (strFile.Left(14).Equals("virtualpath://")) return true;
-  return false;
+  CURL url(strFile);
+
+  return url.GetProtocol() == "virtualpath";
 }
 
 bool CUtil::IsStack(const CStdString& strFile)
 {
-  if (strFile.Left(8).Equals("stack://")) return true;
-  return false;
+  CURL url(strFile);
+
+  return url.GetProtocol() == "stack";
 }
 
 bool CUtil::IsRAR(const CStdString& strFile)
@@ -920,13 +928,25 @@ bool CUtil::IsInArchive(const CStdString &strFile)
 
 bool CUtil::IsInZIP(const CStdString& strFile)
 {
-  CURL url(strFile);
+  CStdString strFile2(strFile);
+
+  if (IsStack(strFile))
+    strFile2 = CStackDirectory::GetFirstStackedFile(strFile);
+
+  CURL url(strFile2);
+
   return url.GetProtocol() == "zip" && url.GetFileName() != "";
 }
 
 bool CUtil::IsInRAR(const CStdString& strFile)
 {
-  CURL url(strFile);
+  CStdString strFile2(strFile);
+
+  if (IsStack(strFile))
+    strFile2 = CStackDirectory::GetFirstStackedFile(strFile);
+
+  CURL url(strFile2);
+
   return url.GetProtocol() == "rar" && url.GetFileName() != "";
 }
 
@@ -941,7 +961,13 @@ bool CUtil::IsZIP(const CStdString& strFile) // also checks for comic books!
 
 bool CUtil::IsSpecial(const CStdString& strFile)
 {
-  CURL url(strFile);
+  CStdString strFile2(strFile);
+
+  if (IsStack(strFile))
+    strFile2 = CStackDirectory::GetFirstStackedFile(strFile);
+
+  CURL url(strFile2);
+
   return url.GetProtocol().Equals("special");
 }
 
@@ -971,9 +997,28 @@ bool CUtil::IsISO9660(const CStdString& strFile)
 
 bool CUtil::IsSmb(const CStdString& strFile)
 {
-  CURL url(strFile);
-  return url.GetProtocol().Equals("iso9660");
-  return strFile.Left(4).Equals("smb:");
+  CStdString strFile2(strFile);
+
+  if (IsStack(strFile))
+    strFile2 = CStackDirectory::GetFirstStackedFile(strFile);
+
+  CURL url(strFile2);
+
+  return url.GetProtocol().Equals("smb");
+}
+
+bool CUtil::IsFTP(const CStdString& strFile)
+{
+  CStdString strFile2(strFile);
+
+  if (IsStack(strFile))
+    strFile2 = CStackDirectory::GetFirstStackedFile(strFile);
+
+  CURL url(strFile2);
+
+  return url.GetProtocol() == "ftp"  ||
+         url.GetProtocol() == "ftpx" ||
+         url.GetProtocol() == "ftps";
 }
 
 bool CUtil::IsDAAP(const CStdString& strFile)
@@ -2527,23 +2572,6 @@ int CUtil::GMTZoneCalc(int iRescBiases, int iHour, int iMinute, int &iMinuteNew)
     }
   }
   return iHourUTC;
-}
-
-bool CUtil::IsFTP(const CStdString& strFile)
-{
-  CStdString strFile2(strFile);
-
-  if (IsStack(strFile))
-    strFile2 = CStackDirectory::GetFirstStackedFile(strFile);
-
-  CURL url(strFile2);
-
-  if (url.GetProtocol() == "ftp"  ||
-      url.GetProtocol() == "ftpx" ||
-      url.GetProtocol() == "ftps")
-  return true;
-
-  return false;
 }
 
 void CUtil::GetRecursiveListing(const CStdString& strPath, CFileItemList& items, const CStdString& strMask, bool bUseFileDirectories)

xbmc/Util.h
@@ -118,6 +118,7 @@ public:
   static bool GetDirectoryName(const CStdString& strFileName, CStdString& strDescription);
   static bool IsISO9660(const CStdString& strFile);
   static bool IsSmb(const CStdString& strFile);
+  static bool IsFTP(const CStdString& strFile);  
   static bool IsDAAP(const CStdString& strFile);
   static bool IsUPnP(const CStdString& strFile);
   static void GetDVDDriveIcon( const CStdString& strPath, CStdString& strIcon );
@@ -176,7 +177,6 @@ public:
 
   static bool SetSysDateTimeYear(int iYear, int iMonth, int iDay, int iHour, int iMinute);
   static int GMTZoneCalc(int iRescBiases, int iHour, int iMinute, int &iMinuteNew);
-  static bool IsFTP(const CStdString& strFile);
   static void GetSkinThemes(std::vector<CStdString>& vecTheme);
   static void GetRecursiveListing(const CStdString& strPath, CFileItemList& items, const CStdString& strMask, bool bUseFileDirectories=false);
   static void GetRecursiveDirsListing(const CStdString& strPath, CFileItemList& items);