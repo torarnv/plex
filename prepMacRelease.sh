e84051c4ce57d79a03980d1928a4e9daebbbd9a0
Elan Feingold <elan@bluemandrill.com>
2010-01-03 13:53:15 -1000

    Extra fix for new boost

--- prepMacRelease.sh
+++ prepMacRelease.sh
@@ -22,6 +22,7 @@ install_name_tool -change /opt/local/lib/libjpeg.62.dylib @executable_path/../Fr
 install_name_tool -change /opt/local/lib/libvorbis.0.dylib @executable_path/../Frameworks/libvorbis.0.dylib ${PLEX}/Contents/MacOS/Plex
 install_name_tool -change /opt/local/lib/libfaad.0.dylib @executable_path/../Frameworks/libfaad.0.dylib ${PLEX}/Contents/MacOS/Plex
 install_name_tool -change /opt/local/lib/libboost_regex-mt.dylib @executable_path/../Frameworks/libboost_regex-mt.dylib ${PLEX}/Contents/MacOS/Plex
+install_name_tool -change /opt/local/lib/libboost_regex-mt-1_35.dylib @executable_path/../Frameworks/libboost_regex-mt.dylib ${PLEX}/Contents/MacOS/Plex
 
 install_name_tool -change /opt/local/lib/libpcre.0.dylib @executable_path/../Frameworks/libpcre.0.dylib ${PLEX}/Contents/MacOS/Plex
 install_name_tool -change /opt/local/lib/libfontconfig.1.dylib @executable_path/../Frameworks/libfontconfig.1.3.0.dylib ${PLEX}/Contents/MacOS/Plex
