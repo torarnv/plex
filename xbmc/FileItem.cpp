563ebffbd1a7f6d37cbe91110a71e037dea3748f
Elan Feingold <elan@bluemandrill.com>
2010-04-27 15:52:39 -1000

    Better checking for Alexandria.

--- xbmc/FileItem.cpp
+++ xbmc/FileItem.cpp
@@ -847,7 +847,9 @@ bool CFileItem::IsPlexMediaServerLibrary() const
   if (IsPlexMediaServer() == false)
       return false;
   
-  if (m_strPath.Find("/library/parts/") != -1 || m_strPath.Find("/library/sections/") != -1)
+  if (m_strPath.Find("/library/parts/") != -1 || 
+      m_strPath.Find("/library/metadata/") != -1 ||
+      m_strPath.Find("/library/sections/") != -1)
   {
     return true;
   }
