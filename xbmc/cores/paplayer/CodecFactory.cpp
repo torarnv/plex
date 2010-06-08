torarnv/plex

commit 4e2bca63579ddbd406f01c311970dedaf818b805

fixed: make paplayer obey USE_LIBA52_DECODER and USE_LIBDTS_DECODER
Use DVDPlayerCodec instead of libdts/liba52 in paplayer when they are
disabled (default).

git-svn-id: http://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@30910 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/cores/paplayer/CodecFactory.cpp
@@ -46,7 +46,7 @@
 #endif
 #include "URL.h"
 #include "DVDPlayerCodec.h"
-#ifdef HAS_DTS_CODEC
+#ifdef USE_LIBDTS_DECODER
 #include "DTSCodec.h"
 #include "DTSCDDACodec.h"
 #endif
@@ -67,13 +67,17 @@ ICodec* CodecFactory::CreateCodec(const CStdString& strFileType)
     return new FLACCodec();
   else if (strFileType.Equals("wav"))
     return new DVDPlayerCodec();
-#ifdef HAS_DTS_CODEC
   else if (strFileType.Equals("dts"))
+#ifdef USE_LIBDTS_DECODER
     return new DTSCodec();
+#else
+    return new DVDPlayerCodec();
 #endif
-#ifdef HAS_AC3_CODEC
   else if (strFileType.Equals("ac3"))
+#ifdef USE_LIBA52_DECODER
     return new AC3Codec();
+#else
+    return new DVDPlayerCodec();
 #endif
   else if (strFileType.Equals("m4a") || strFileType.Equals("aac"))
     return new DVDPlayerCodec();
@@ -153,7 +157,7 @@ ICodec* CodecFactory::CreateCodecDemux(const CStdString& strFile, const CStdStri
   if (urlFile.GetFileType().Equals("wav"))
   {
     ICodec* codec;
-#ifdef HAS_DTS_CODEC
+#ifdef USE_LIBDTS_DECODER
     //lets see what it contains...
     //this kinda sucks 'cause if it's a plain wav file the file
     //will be opened, sniffed and closed 2 times before it is opened *again* for wav
@@ -165,8 +169,16 @@ ICodec* CodecFactory::CreateCodecDemux(const CStdString& strFile, const CStdStri
     }
     delete codec;
 #endif
-#ifdef HAS_AC3_CODEC
+#ifdef USE_LIBA52_DECODER
     codec = new AC3Codec();
+    if (codec->Init(strFile, filecache))
+    {
+      return codec;
+    }
+    delete codec;
+#endif
+#if !defined(USE_LIBDTS_DECODER) || !defined(USE_LIBA52_DECODER)
+    codec = new DVDPlayerCodec();
     if (codec->Init(strFile, filecache))
     {
       return codec;
@@ -189,12 +201,13 @@ ICodec* CodecFactory::CreateCodecDemux(const CStdString& strFile, const CStdStri
   }
   if (urlFile.GetFileType().Equals("cdda"))
   {
-#ifdef HAS_DTS_CODEC
+    ICodec* codec;
+#ifdef USE_LIBDTS_DECODER
     //lets see what it contains...
     //this kinda sucks 'cause if it's plain cdda the file
     //will be opened, sniffed and closed 2 times before it is opened *again* for cdda
     //would be better if the papcodecs could work with bitstreams instead of filenames.
-    ICodec* codec = new DTSCDDACodec();
+    codec = new DTSCDDACodec();
     if (codec->Init(strFile, filecache))
     {
       return codec;
@@ -203,6 +216,14 @@ ICodec* CodecFactory::CreateCodecDemux(const CStdString& strFile, const CStdStri
 #endif
 #ifdef HAS_AC3_CDDA_CODEC
     codec = new AC3CDDACodec();
+    if (codec->Init(strFile, filecache))
+    {
+      return codec;
+    }
+    delete codec;
+#endif
+#if !defined(USE_LIBDTS_DECODER) || !defined(HAS_AC3_CDDA_CODEC)
+    codec = new DVDPlayerCodec();
     if (codec->Init(strFile, filecache))
     {
       return codec;

guilib/system.h
@@ -53,8 +53,6 @@
 #define HAS_JSONRPC
 #define HAS_HTTPAPI
 
-#define HAS_AC3_CODEC
-#define HAS_DTS_CODEC
 #define HAS_CDDA_RIPPER
 #ifdef HAVE_ASAP_CODEC
 #define HAS_ASAP_CODEC

xbmc/cores/paplayer/AC3CDDACodec.h
@@ -23,7 +23,7 @@
 
 #include "AC3Codec.h"
 
-#ifdef HAS_AC3_CODEC
+#ifdef USE_LIBA52_DECODER
 class AC3CDDACodec : public AC3Codec
 {
 public:

xbmc/cores/paplayer/AC3Codec.cpp
@@ -23,7 +23,7 @@
 #include "AC3Codec.h"
 #include "utils/log.h"
 
-#ifdef HAS_AC3_CODEC
+#ifdef USE_LIBA52_DECODER
 
 static inline int16_t convert(int32_t i)
 {

xbmc/cores/paplayer/AC3Codec.h
@@ -24,7 +24,7 @@
 #include "CachingCodec.h"
 #include "DllAc3codec.h"
 
-#ifdef HAS_AC3_CODEC
+#ifdef USE_LIBA52_DECODER
 
 class AC3Codec : public CachingCodec
 {

xbmc/cores/paplayer/DTSCDDACodec.cpp
@@ -21,7 +21,7 @@
 
 #include "system.h"
 #include "DTSCDDACodec.h"
-#ifdef HAS_DTS_CODEC
+#ifdef USE_LIBDTS_DECODER
 #include <cdio/sector.h>
 
 DTSCDDACodec::DTSCDDACodec() : DTSCodec()

xbmc/cores/paplayer/DTSCDDACodec.h
@@ -23,7 +23,7 @@
 
 #include "DTSCodec.h"
 
-#ifdef HAS_DTS_CODEC
+#ifdef USE_LIBDTS_DECODER
 class DTSCDDACodec : public DTSCodec
 {
 public:

xbmc/cores/paplayer/DTSCodec.cpp
@@ -21,7 +21,7 @@
 
 #include "system.h"
 #include "utils/log.h"
-#ifdef HAS_DTS_CODEC
+#ifdef USE_LIBDTS_DECODER
 #include "DTSCodec.h"
 
 static inline int16_t convert(int32_t i)

commit 5b0f98d377d0b9386c7c4a64005b4598da63b49b

fixed: cdda playback and dts-in-wav playback
CDDA playback regressed in r30910, while dts-in-wav was broken even
before that. Also tries to handle ac3-in-wav, but not sure if it
works.

git-svn-id: http://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@30959 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/cores/paplayer/CodecFactory.cpp
@@ -168,6 +168,13 @@ ICodec* CodecFactory::CreateCodecDemux(const CStdString& strFile, const CStdStri
       return codec;
     }
     delete codec;
+#else
+    codec = new DVDPlayerCodec(CODEC_ID_DTS);
+    if (codec->Init(strFile, filecache))
+    {
+      return codec;
+    }
+    delete codec;
 #endif
 #ifdef USE_LIBA52_DECODER
     codec = new AC3Codec();
@@ -176,9 +183,8 @@ ICodec* CodecFactory::CreateCodecDemux(const CStdString& strFile, const CStdStri
       return codec;
     }
     delete codec;
-#endif
-#if (!defined(USE_LIBDTS_DECODER) || !defined(USE_LIBA52_DECODER)) && !defined(WIN32)
-    codec = new DVDPlayerCodec();
+#else
+    codec = new DVDPlayerCodec(CODEC_ID_AC3);
     if (codec->Init(strFile, filecache))
     {
       return codec;
@@ -213,6 +219,13 @@ ICodec* CodecFactory::CreateCodecDemux(const CStdString& strFile, const CStdStri
       return codec;
     }
     delete codec;
+#else
+    codec = new DVDPlayerCodec(CODEC_ID_DTS);
+    if (codec->Init(strFile, filecache))
+    {
+      return codec;
+    }
+    delete codec;
 #endif
 #ifdef HAS_AC3_CDDA_CODEC
     codec = new AC3CDDACodec();
@@ -221,9 +234,8 @@ ICodec* CodecFactory::CreateCodecDemux(const CStdString& strFile, const CStdStri
       return codec;
     }
     delete codec;
-#endif
-#if (!defined(USE_LIBDTS_DECODER) || !defined(HAS_AC3_CDDA_CODEC)) && !defined(WIN32)
-    codec = new DVDPlayerCodec();
+#else
+    codec = new DVDPlayerCodec(CODEC_ID_AC3);
     if (codec->Init(strFile, filecache))
     {
       return codec;

xbmc/cores/paplayer/DVDPlayerCodec.cpp
@@ -31,7 +31,7 @@
 
 #include "AudioDecoder.h"
 
-DVDPlayerCodec::DVDPlayerCodec()
+DVDPlayerCodec::DVDPlayerCodec(CodecID codec)
 {
   m_CodecName = "DVDPlayer";
   m_pDemuxer = NULL;
@@ -41,6 +41,7 @@ DVDPlayerCodec::DVDPlayerCodec()
   m_pPacket = NULL;
   m_decoded = NULL;;
   m_nDecodedLen = 0;
+  m_codec = codec;
 }
 
 DVDPlayerCodec::~DVDPlayerCodec()
@@ -129,6 +130,13 @@ bool DVDPlayerCodec::Init(const CStdString &strFile, unsigned int filecache)
   }
 
   CDVDStreamInfo hint(*pStream, true);
+  if (m_codec) {
+    hint.codec = m_codec;
+    hint.channels = 0;
+    hint.samplerate = 0;
+    hint.bitspersample = 0;
+  }
+
   m_pAudioCodec = CDVDFactoryCodec::CreateAudioCodec(hint);
   if (!m_pAudioCodec)
   {
@@ -142,17 +150,24 @@ bool DVDPlayerCodec::Init(const CStdString &strFile, unsigned int filecache)
 
   // we have to decode initial data in order to get channels/samplerate
   // for sanity - we read no more than 10 packets
+  int nErrors = 0;
   for (int nPacket=0; nPacket < 10 && (m_Channels == 0 || m_SampleRate == 0); nPacket++)
   {
     BYTE dummy[256];
     int nSize = 256;
-    ReadPCM(dummy, nSize, &nSize);
+    if (ReadPCM(dummy, nSize, &nSize) == READ_ERROR)
+      ++nErrors;
 
     // We always ask ffmpeg to return s16le
     m_BitsPerSample = m_pAudioCodec->GetBitsPerSample();
     m_SampleRate = m_pAudioCodec->GetSampleRate();
     m_Channels = m_pAudioCodec->GetChannels();
 
+  }
+  if (nErrors >= 10)
+  {
+    CLog::Log(LOGDEBUG, "%s: Could not decode data", __FUNCTION__);
+    return false;
   }
 
   m_nDecodedLen = 0;

xbmc/cores/paplayer/DVDPlayerCodec.h
@@ -32,7 +32,7 @@
 class DVDPlayerCodec : public ICodec
 {
 public:
-  DVDPlayerCodec();
+  DVDPlayerCodec(CodecID codec = CODEC_ID_NONE);
   virtual ~DVDPlayerCodec();
 
   virtual bool Init(const CStdString &strFile, unsigned int filecache);
@@ -58,6 +58,8 @@ private:
 
   BYTE *m_decoded;
   int  m_nDecodedLen;
+  
+  CodecID m_codec;
 };
 
 #endif