torarnv/plex

commit 65626e5faa73c9e877beac23cd75a57621bfa980

fixed: XBMC python module with non-wchar_t unicode python
Python can use either UCS2 or UCS4 for its unicode type, depending on
compilation options. By default it uses UCS2.

Currently we convert between unicode<->utf8 on the assumption that
python unicode characters are the same size as wchar_t, which is 16 bits
on win32 and 32 bits on linux. This causes breakage when using external
python compiled with the default compilation options.

Fix that by using the appropriate python functions for converting
between unicode<=>utf8 instead of doing the conversion ourselves.

git-svn-id: http://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@31980 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/win32/WIN32XBPythonDll.cpp
@@ -195,9 +195,11 @@ extern "C"
     return ret;
   }*/
 #ifndef _LINUX
-  FUNCTION4(PyUnicodeUCS2_AsUnicode)
+  FUNCTION4(PyUnicodeUCS2_AsUTF8String)
+  FUNCTION4(PyUnicodeUCS2_DecodeUTF8)
 #else
-  FUNCTION4(PyUnicodeUCS4_AsUnicode)
+  FUNCTION4(PyUnicodeUCS4_AsUTF8String)
+  FUNCTION4(PyUnicodeUCS4_DecodeUTF8)
 #endif
   FUNCTION(Py_MakePendingCalls)
   FUNCTION(PyEval_SaveThread)
@@ -320,9 +322,11 @@ extern "C"
       dll.ResolveExport(DLL_FUNCTION(PyLong_AsLongLong)) &&
       dll.ResolveExport(DLL_VA_FUNCTION(PyErr_Format)) &&
 #ifndef _LINUX
-      dll.ResolveExport(DLL_FUNCTION(PyUnicodeUCS2_AsUnicode)) &&
+      dll.ResolveExport(DLL_FUNCTION(PyUnicodeUCS2_AsUTF8String)) &&
+      dll.ResolveExport(DLL_FUNCTION(PyUnicodeUCS2_DecodeUTF8)) &&
 #else
-      dll.ResolveExport(DLL_FUNCTION(PyUnicodeUCS4_AsUnicode)) &&
+      dll.ResolveExport(DLL_FUNCTION(PyUnicodeUCS4_AsUTF8String)) &&
+      dll.ResolveExport(DLL_FUNCTION(PyUnicodeUCS4_DecodeUTF8)) &&
 #endif
       dll.ResolveExport(DLL_FUNCTION(Py_MakePendingCalls)) &&
       dll.ResolveExport(DLL_FUNCTION(PyEval_SaveThread)) &&

xbmc/lib/libPython/XBPythonDll.cpp
@@ -98,9 +98,11 @@ extern "C"
   FUNCTION(PyLong_AsLongLong)
   FUNCTION(PyErr_Format)
 #if Py_UNICODE_SIZE == 2
-  FUNCTION(PyUnicodeUCS2_AsUnicode)
+  FUNCTION(PyUnicodeUCS2_AsUTF8String)
+  FUNCTION(PyUnicodeUCS2_DecodeUTF8)
 #else
-  FUNCTION(PyUnicodeUCS4_AsUnicode)
+  FUNCTION(PyUnicodeUCS4_AsUTF8String)
+  FUNCTION(PyUnicodeUCS4_DecodeUTF8)
 #endif
   FUNCTION(Py_MakePendingCalls)
   FUNCTION(PyEval_SaveThread)
@@ -211,9 +213,11 @@ extern "C"
       dll.ResolveExport(DLL_FUNCTION(PyLong_AsLongLong)) &&
       dll.ResolveExport(DLL_FUNCTION(PyErr_Format)) &&
 #if Py_UNICODE_SIZE == 2
-      dll.ResolveExport(DLL_FUNCTION(PyUnicodeUCS2_AsUnicode)) &&
+      dll.ResolveExport(DLL_FUNCTION(PyUnicodeUCS2_AsUTF8String)) &&
+      dll.ResolveExport(DLL_FUNCTION(PyUnicodeUCS2_DecodeUTF8)) &&
 #else
-      dll.ResolveExport(DLL_FUNCTION(PyUnicodeUCS4_AsUnicode)) &&
+      dll.ResolveExport(DLL_FUNCTION(PyUnicodeUCS4_AsUTF8String)) &&
+      dll.ResolveExport(DLL_FUNCTION(PyUnicodeUCS4_DecodeUTF8)) &&
 #endif
       dll.ResolveExport(DLL_FUNCTION(Py_MakePendingCalls)) &&
       dll.ResolveExport(DLL_FUNCTION(PyEval_SaveThread)) &&

xbmc/lib/libPython/XBPythonDllFuncs.S
@@ -99,9 +99,11 @@
   FUNCTION(PyLong_AsLongLong)
   FUNCTION(PyErr_Format)
 #if Py_UNICODE_SIZE == 2
-  FUNCTION(PyUnicodeUCS2_AsUnicode)
+  FUNCTION(PyUnicodeUCS2_AsUTF8String)
+  FUNCTION(PyUnicodeUCS2_DecodeUTF8)
 #else
-  FUNCTION(PyUnicodeUCS4_AsUnicode)
+  FUNCTION(PyUnicodeUCS4_AsUTF8String)
+  FUNCTION(PyUnicodeUCS4_DecodeUTF8)
 #endif
   FUNCTION(Py_MakePendingCalls)
   FUNCTION(PyEval_SaveThread)

xbmc/lib/libPython/xbmcmodule/PythonAddon.cpp
@@ -22,7 +22,6 @@
 #include "PythonAddon.h"
 #include "pyutil.h"
 #include "addons/AddonManager.h"
-#include "utils/CharsetConverter.h"
 #include "GUIDialogAddonSettings.h"
 
 #ifndef __GNUC__
@@ -103,10 +102,9 @@ namespace PYXBMC
       return NULL;
     };
 
-    CStdStringW label;
-    g_charsetConverter.utf8ToW(self->pAddon->GetString(id), label);
+    CStdString label = self->pAddon->GetString(id);
 
-    return Py_BuildValue((char*)"u", label.c_str());
+    return PyUnicode_DecodeUTF8(label.c_str(), label.size(), "replace");
   }
 
     PyDoc_STRVAR(getSetting__doc__,

xbmc/lib/libPython/xbmcmodule/controlbutton.cpp
@@ -19,7 +19,6 @@
  *
  */
 
-#include "utils/CharsetConverter.h"
 #if (defined HAVE_CONFIG_H) && (!defined WIN32)
   #include "config.h"
 #endif
@@ -304,11 +303,10 @@ namespace PYXBMC
     if (!self->pGUIControl) return NULL;
 
     PyXBMCGUILock();
-    CStdStringW label;
-    g_charsetConverter.utf8ToW(((CGUIButtonControl*) self->pGUIControl)->GetLabel(), label);
+    CStdString label = ((CGUIButtonControl*) self->pGUIControl)->GetLabel();
     PyXBMCGUIUnlock();
 
-    return Py_BuildValue((char*)"u", label.c_str());
+    return PyUnicode_DecodeUTF8(label.c_str(), label.size(), "replace");
   }
 
   // getLabel2() Method
@@ -323,11 +321,10 @@ namespace PYXBMC
     if (!self->pGUIControl) return NULL;
 
     PyXBMCGUILock();
-    CStdStringW label;
-    g_charsetConverter.utf8ToW(((CGUIButtonControl*) self->pGUIControl)->GetLabel2(), label);
+    CStdString label = ((CGUIButtonControl*) self->pGUIControl)->GetLabel2();
     PyXBMCGUIUnlock();
 
-    return Py_BuildValue((char*)"u", label.c_str());
+    return PyUnicode_DecodeUTF8(label.c_str(), label.size(), "replace");
   }
 
   PyMethodDef ControlButton_methods[] = {

xbmc/lib/libPython/xbmcmodule/controlradiobutton.cpp
@@ -37,7 +37,6 @@
 #endif
 #include "../XBPythonDll.h"
 #include "GUIRadioButtonControl.h"
-#include "utils/CharsetConverter.h"
 #include "GUIFontManager.h"
 #include "control.h"
 #include "pyutil.h"

xbmc/lib/libPython/xbmcmodule/pyutil.cpp
@@ -49,17 +49,18 @@ namespace PYXBMC
     //              for non-unicode data?
     if (PyUnicode_Check(pObject))
     {
-      // this will probably not really work since the python DLL assumes that
-      // that wchar_t is 2 bytes and linux is actually 4 bytes. That's why
-      // so building a CStdStringW will not work
-      //
-      CStdString utf8String;
 
-      CStdStringW utf16String = (wchar_t*) PyUnicode_AsUnicode(pObject);
-      g_charsetConverter.wToUTF8(utf16String, utf8String);
+      // Python unicode objects are UCS2 or UCS4 depending on compilation
+      // options, wchar_t is 16-bit or 32-bit depending on platform.
+      // Avoid the complexity by just letting python convert the string.
+      PyObject *utf8_pyString = PyUnicode_AsUTF8String(pObject);
 
-      buf = utf8String;
-      return 1;
+      if (utf8_pyString)
+      {
+        buf = PyString_AsString(utf8_pyString);
+        Py_DECREF(utf8_pyString);
+        return 1;
+      }
     }
     if (PyString_Check(pObject))
     {

xbmc/lib/libPython/xbmcmodule/xbmcmodule.cpp
@@ -408,15 +408,15 @@ namespace PYXBMC
     int iString;
     if (!PyArg_ParseTuple(args, (char*)"i", &iString)) return NULL;
 
-    CStdStringW unicodeLabel;
+    CStdString label;
     if (iString >= 30000 && iString <= 30999)
-      g_charsetConverter.utf8ToW(g_localizeStringsTemp.Get(iString), unicodeLabel);
+      label = g_localizeStringsTemp.Get(iString);
     else if (iString >= 32000 && iString <= 32999)
-      g_charsetConverter.utf8ToW(g_localizeStringsTemp.Get(iString), unicodeLabel);
+      label = g_localizeStringsTemp.Get(iString);
     else
-      g_charsetConverter.utf8ToW(g_localizeStrings.Get(iString), unicodeLabel);
+      label = g_localizeStrings.Get(iString);
 
-    return Py_BuildValue((char*)"u", unicodeLabel.c_str());
+    return PyUnicode_DecodeUTF8(label.c_str(), label.size(), "replace");
   }
 
   // getSkinDir() method

commit 7ec2c2c2f249d64a280090ede9f3c96a8aeb0d9b

[WIN32] fixed: python regression in r31980
Incorrect parameter count was used for PyUnicode_DecodeUTF8.

git-svn-id: http://xbmc.svn.sourceforge.net/svnroot/xbmc/trunk@31984 568bbfeb-2a22-0410-94d2-cc84cf5bfa90

xbmc/win32/WIN32XBPythonDll.cpp
@@ -196,10 +196,10 @@ extern "C"
   }*/
 #ifndef _LINUX
   FUNCTION4(PyUnicodeUCS2_AsUTF8String)
-  FUNCTION4(PyUnicodeUCS2_DecodeUTF8)
+  FUNCTION12(PyUnicodeUCS2_DecodeUTF8)
 #else
   FUNCTION4(PyUnicodeUCS4_AsUTF8String)
-  FUNCTION4(PyUnicodeUCS4_DecodeUTF8)
+  FUNCTION12(PyUnicodeUCS4_DecodeUTF8)
 #endif
   FUNCTION(Py_MakePendingCalls)
   FUNCTION(PyEval_SaveThread)